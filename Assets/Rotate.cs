ddd8d3642ceef821283e070a413262fdfb72005c
NikThomas9 <nikrthomas@gmail.com>
2022-10-19 15:29:07 -0700

    bang bing bing

---
+++ Assets/Rotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateX * Time.deltaTime, 
+        rotateY * Time.deltaTime, 
+        rotateZ * Time.deltaTime);
+    }
+}
